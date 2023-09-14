2601ccad3516de13f5322adc3149cee3ebeed123
Georgey <62781373+jetsup@users.noreply.github.com>
2023-09-14 21:35:14 +0300

    first commit

---
+++ components/navbar.php
@@ -0,0 +1,9 @@
+<header>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products and Services</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Contacts</a></li>
+        </ul>
+    </header>
---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jack Daniels - Portfolio</title>
+</head>
+<body>
+    <h1>Homepage</h1>
+    <?php include 'components/navbar.php'; ?>
+</body>
+<script src="index.js"></script>
+</html>
